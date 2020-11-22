27e4cf3c51af668562582fe7874bfe3ca5b98aff
Marko Lohert <marko_lohert@yahoo.com>
2020-11-22 23:59:34 +0100

    Top-level statements used in ReportVacationDays.

--- Demo/CLI Tools/ReportVacationDays/ReportVacationDays/Program.cs
+++ Demo/CLI Tools/ReportVacationDays/ReportVacationDays/Program.cs
@@ -1,20 +1,10 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
-
-namespace ReportVacationDays
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            using SqlConnection connection = new SqlConnection(ReportVacationDays.Properties.Resources.ConnectionString);
-            using SqlDataAdapter adapter = new SqlDataAdapter();
-            adapter.SelectCommand = new SqlCommand("SELECT LastName, FirstName, VacationDays FROM Employee ORDER BY LastName, FirstName", connection);
-            using DataTable dataTable = new DataTable();
-            adapter.Fill(dataTable);
-            string reportFileName = $"C:\\HR #\\Report Vacation Days {DateTime.Now:yyyy-MM-dd hh.mm}.xlsx";
-            DataTableToExcel.DataTableToExcel.ExportToExcel(reportFileName, WworkBookName: "HR #", dataTable);
-        }
-    }
-}
+using SqlConnection connection = new SqlConnection(ReportVacationDays.Properties.Resources.ConnectionString);
+using SqlDataAdapter adapter = new SqlDataAdapter();
+adapter.SelectCommand = new SqlCommand("SELECT LastName, FirstName, VacationDays FROM Employee ORDER BY LastName, FirstName", connection);
+using DataTable dataTable = new DataTable();
+adapter.Fill(dataTable);
+string reportFileName = $"C:\\HR #\\Report Vacation Days {DateTime.Now:yyyy-MM-dd hh.mm}.xlsx";
+DataTableToExcel.DataTableToExcel.ExportToExcel(reportFileName, WworkBookName: "HR #", dataTable);
