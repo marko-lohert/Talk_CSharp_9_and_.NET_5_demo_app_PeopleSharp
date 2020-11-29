440b4efe85976399609da76f3a29f33d2ba3c894
Marko Lohert <lohert.marko@gmail.com>
2020-11-29 22:00:47 +0100

    Statistics added (#1)
    * Statistics (Company, Employees) added. A page CompanyStatistics added.
    
    * Statistics shared classes moved into a separate namespace and folder.

---
+++ Demo/HRSharp/HRSharp/Server/Statistics/StatsCompanyCalculator.cs
@@ -0,0 +1,28 @@
+﻿using HRSharp.Server.Controllers;
+using HRSharp.Shared;
+using HRSharp.Shared.Statistics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HRSharp.Server.Statistics
+{
+    public class StatsCompanyCalculator : StatsEmployeesCalculator
+    {
+        public StatsCompany CalculateStatsCompany()
+        {
+            DepartmentsController departmentsController = new DepartmentsController();
+            List<Department>? allDepartments = departmentsController.GetAllDepartments()?.ToList();
+
+            int countDepartments = allDepartments?.Count ?? 0;
+
+            // todo Count all managers (replace mocked data).
+            // Mock data
+            int countManagers = 4;
+
+            StatsEmployeesCalculator statsEmployeesCalculator = new StatsEmployeesCalculator();
+            StatsEmployees statsEmployees = statsEmployeesCalculator.CalculateStats();
+
+            return new StatsCompany(countDepartments, countManagers, statsEmployees.CountEmployees, statsEmployees.AvgVacationDays, statsEmployees.DiffJobTitles);
+        }
+    }
+}
---
+++ Demo/HRSharp/HRSharp/Shared/Statistics/StatsEmployees.cs
@@ -0,0 +1,55 @@
+﻿namespace HRSharp.Shared.Statistics
+{
+    public class StatsEmployees
+    {
+        public int CountEmployees { get; set; }
+        public decimal AvgVacationDays { get; set; }
+        public int DiffJobTitles { get; set; }
+
+        public StatsEmployees(int countEmployees, decimal avgVacationDays, int diffJobTitles)
+        {
+            CountEmployees = countEmployees;
+            AvgVacationDays = avgVacationDays;
+            DiffJobTitles = diffJobTitles;
+        }
+        
+        public void Deconstructor(out int countEmployees, out decimal avgVacationDays, out int diffJobTitles)
+        {
+            countEmployees = CountEmployees;
+            avgVacationDays = AvgVacationDays;
+            diffJobTitles = DiffJobTitles;
+        }
+
+        public override bool Equals(object? o)
+        {
+            return o != null &&
+                   o is StatsEmployees other &&
+                   CountEmployees.Equals(other.CountEmployees) &&
+                   AvgVacationDays.Equals(other.AvgVacationDays) &&
+                   DiffJobTitles.Equals(other.DiffJobTitles);
+        }
+
+        public override int GetHashCode()
+        {
+            return CountEmployees.GetHashCode() + AvgVacationDays.GetHashCode() + DiffJobTitles.GetHashCode();
+        }
+
+        public static bool operator==(StatsEmployees left, StatsEmployees right)
+        {
+            if (left is object)
+                return left.Equals(right);
+            else
+                return right is null;
+        }
+
+        public static bool operator!=(StatsEmployees left, StatsEmployees right)
+        {
+            return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return $"A number of employees: {CountEmployees}. An average number of vacation days: {AvgVacationDays}. A number of different job titles: {DiffJobTitles}";
+        }
+    }
+}
